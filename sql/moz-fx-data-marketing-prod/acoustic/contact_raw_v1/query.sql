4e9ed124d2ac37cc74d6524d0372efd2c615562e
kik-kik <kignasiak01@gmail.com>
2022-04-14 15:16:46 +0200

    changed query for acoustic tables

--- sql/moz-fx-data-marketing-prod/acoustic/contact_raw_v1/query.sql
+++ sql/moz-fx-data-marketing-prod/acoustic/contact_raw_v1/query.sql
@@ -1,3 +1,11 @@
--- TODO
+BEGIN
+    SELECT
+        *,  -- if new fields are added this should break and require to manual schema change
+        @submission_date AS submission_date,
 
-SELECT 1
+    FROM
+        `dev-fivetran.acoustic_sftp.contact_export`;
+
+    TRUNCATE TABLE `dev-fivetran.acoustic_sftp.contact_export`;
+
+END;
--- sql/moz-fx-data-marketing-prod/acoustic/raw_recipient_v1/query.sql
+++ sql/moz-fx-data-marketing-prod/acoustic/raw_recipient_v1/query.sql
@@ -1,3 +1,11 @@
--- TODO
+BEGIN
+    SELECT
+        *,  -- if new fields are added this should break and require to manual schema change
+        @submission_date AS submission_date,
 
-SELECT 1
+    FROM
+        `dev-fivetran.acoustic_sftp.raw_recipient_export`;
+
+    TRUNCATE TABLE IF EXISTS `dev-fivetran.acoustic_sftp.raw_recipient_export`;
+
+END;
