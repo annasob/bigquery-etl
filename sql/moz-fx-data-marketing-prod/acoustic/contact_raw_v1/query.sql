46de8a8070f32fea712fc5069a54f76a82f7ed6d
kik-kik <kignasiak01@gmail.com>
2022-04-19 11:04:18 +0200

    tweaked logic to do some processing/cleaning before loading into user facing table

--- sql/moz-fx-data-marketing-prod/acoustic/contact_raw_v1/query.sql
+++ sql/moz-fx-data-marketing-prod/acoustic/contact_raw_v1/query.sql
@@ -1,46 +1,53 @@
 BEGIN
 
     SELECT
-        email,
-        basket_token,
-        sfdc_id,
-        double_opt_in,
-        has_opted_out_of_email,
-        email_format,
-        email_lang,
-        fxa_created_date,
-        fxa_first_service,
-        fxa_id,
-        fxa_account_deleted,
-        email_id,
-        mailing_country,
-        cohort,
-        sub_mozilla_foundation,
-        sub_common_voice,
-        sub_hubs,
-        sub_mixed_reality,
-        sub_internet_health_report,
-        sub_miti,
-        sub_mozilla_fellowship_awardee_alumni,
-        sub_mozilla_festival,
-        sub_mozilla_technology,
-        sub_mozillians_nda,
-        sub_firefox_accounts_journey,
-        sub_knowledge_is_power,
-        sub_take_action_for_the_internet,
-        sub_test_pilot,
-        sub_firefox_news,
-        vpn_waitlist_geo,
-        vpn_waitlist_platform,
-        sub_about_mozilla,
-        sub_apps_and_hacks,
-        sub_rally,
-        sub_firefox_sweepstakes,
-        relay_waitlist_geo,
-        recipient_id,
-        last_modified_date,
-        @submission_date AS ,
-        CURRENT_DATETIME() AS loaded_at,
+        -- LOWER(email),  -- do we actually need email? Looks like we could use email_id as a unique identifier? -- not null // string
+        email_id,  -- not null // string
+        COALESCE(basket_token, "unknown") AS basket_token,  -- 8729 so far with null values | is this a problem?  // string
+        COALESCE(sfdc_id, "unknown") AS sfdc_id,  -- 1142993 so far with null values | is this a problem?  // string
+        COALESCE(fxa_id, "unknown") AS fxa_id,  -- 121606 nulls || this is bad?
+
+        CAST(COALESCE(double_opt_in, 0) AS BOOLEAN) AS double_opt_in,  -- no nulls, true or false value  // boolean
+        CAST(COALESCE(has_opted_out_of_email, 0) AS BOOLEAN) AS has_opted_out_of_email,  -- int, no nulls, true or false value  // boolean
+
+        LOWER(COALESCE(email_lang, "unknown")) AS email_lang, --  6126 nulls, and case differences  // string
+        LOWER(email_format) AS email_format,  -- no nulls, CHAR (T OR H)  // string
+        LOWER(COALESCE(mailing_country, "unknown")) AS mailing_country,  -- no nulls  // string
+        LOWER(COALESCE(cohort, "unknown")) AS cohort,  -- 1320918 nulls
+
+        fxa_created_date AS fxa_created_date,  -- 107057 nulls  -- need to parse the date
+        LOWER(COALESCE(fxa_first_service, "unknown")) AS fxa_first_service,  -- no nulls  // string
+        CAST(COALESCE(fxa_account_deleted, 0) AS BOOLEAN) AS fxa_account_deleted,  -- 77207 nulls // boolean but contains nulls | how should they be treated?
+
+        CAST(COALESCE(sub_mozilla_foundation, 0) AS BOOLEAN) AS sub_mozilla_foundation,  -- 2 nulls  // boolean
+        CAST(COALESCE(sub_common_voice, 0) AS BOOLEAN) AS sub_common_voice,  -- 2 nulls  // boolean
+        CAST(COALESCE(sub_hubs, 0) AS BOOLEAN) AS sub_hubs,  -- 2 nulls  // boolean
+        CAST(COALESCE(sub_mixed_reality, 0) AS BOOLEAN) AS sub_mixed_reality,  -- 2 nulls  // boolean
+        CAST(COALESCE(sub_internet_health_report, 0) AS BOOLEAN) AS sub_internet_health_report,  -- # nulls  // boolean
+        CAST(COALESCE(sub_miti, 0) AS BOOLEAN) AS sub_miti,  -- 2 nulls  // boolean
+        CAST(COALESCE(sub_mozilla_fellowship_awardee_alumni, 0) AS BOOLEAN) AS sub_mozilla_fellowship_awardee_alumni,  -- # nulls  // boolean
+        CAST(COALESCE(sub_mozilla_festival, 0) AS BOOLEAN) AS sub_mozilla_festival,  -- 2 nulls  // boolean
+        CAST(COALESCE(sub_mozilla_technology, 0) AS BOOLEAN) AS sub_mozilla_technology,  -- 2 nulls  // boolean
+        CAST(COALESCE(sub_mozillians_nda, 0) AS BOOLEAN) AS sub_mozillians_nda,  -- 2 nulls  // boolean
+        CAST(COALESCE(sub_firefox_accounts_journey, 0) AS BOOLEAN) AS sub_firefox_accounts_journey,  -- 2 nulls  // boolean
+        CAST(COALESCE(sub_knowledge_is_power, 0) AS BOOLEAN) AS sub_knowledge_is_power,  -- 2 nulls  // boolean
+        CAST(COALESCE(sub_take_action_for_the_internet, 0) AS BOOLEAN) AS sub_take_action_for_the_internet,  -- 2 nulls  // boolean
+        CAST(COALESCE(sub_test_pilot, 0) AS BOOLEAN) AS sub_test_pilot,  -- 2 nulls  // boolean
+        CAST(COALESCE(sub_firefox_news, 0) AS BOOLEAN) AS sub_firefox_news,  -- 2 nulls  // boolean
+        CAST(COALESCE(sub_about_mozilla, 0) AS BOOLEAN) AS sub_about_mozilla,  -- 13436 nulls  // boolean
+        CAST(COALESCE(sub_apps_and_hacks, 0) AS BOOLEAN) AS sub_apps_and_hacks,  -- 12988 nulls  // boolean
+        CAST(COALESCE(sub_rally, 0) AS BOOLEAN) AS sub_rally,  -- 13641 nulls  // boolean
+        CAST(COALESCE(sub_firefox_sweepstakes, 0) AS BOOLEAN) AS sub_firefox_sweepstakes,  -- 14226 nulls  // boolean
+
+        -- TODO: assuming 0 for us means false which means NOT subscribed
+
+        LOWER(vpn_waitlist_geo) AS vpn_waitlist_geo,  -- 1221463 nulls, contains case diffences  // no_value is a bad choice of default, should this just be null?
+        SPLIT(vpn_waitlist_platform, ",") AS vpn_waitlist_platform,  -- 1230937 nulls, single field contains multiple values comma separated, should this be a repeated field?  // repeated string field
+        -- relay_waitlist_geo,  -- # missing field, need to verify, maybe it's related to reusing old connector
+        -- recipient_id,  -- 1326878 nulls, a field coming from Acoustic, shoudl this field be excluded?
+        last_modified_date,  -- 1326878 nulls, acoustic field
+        @submission_date AS job_date,  -- // datetime
+        CURRENT_DATETIME() AS processed_at, --  //  datetime
 
     FROM
         `dev-fivetran.acoustic_sftp.raw_recipient_export`;
--- sql/moz-fx-data-marketing-prod/acoustic/raw_recipient_v1/query.sql
+++ sql/moz-fx-data-marketing-prod/acoustic/raw_recipient_v1/query.sql
@@ -1,19 +1,19 @@
 BEGIN
 
     SELECT
-        email,
-        body_type,
-        click_name,
-        event_timestamp,
-        event_type,
-        mailing_id,
-        recipient_id,
-        recipient_type,
-        report_id,
-        url,
-        suppression_reason,
-        @submission_date AS job_date,
-        CURRENT_DATETIME() AS loaded_at,
+        email,  -- no nulls  // string
+        recipient_id,  -- no nulls  // int
+        report_id,  -- no nulls  // int
+        mailing_id,  -- no null // int
+        PARSE_DATETIME("%m/%d/%Y %H:%M:%S", event_timestamp) AS event_timestamp, -- no nulls  // datetime example: 04/14/2022 12:02:50
+        LOWER(COALESCE(event_type, "unknown")) AS event_type, -- no nulls // string
+        LOWER(COALESCE(recipient_type, "unknown")) AS recipient_type, -- no nulls // string
+        LOWER(COALESCE(body_type, "unknown")) AS body_type, -- 7441275 nulls // string
+        LOWER(COALESCE(click_name, "unknown")) AS click_name,  -- 9075718 nulls // string
+        COALESCE(url, "unknown") AS url,  -- 9075718 nulls // string
+        LOWER(COALESCE(suppression_reason, "unknown")) AS suppression_reason,  -- 8456720 nulls // string
+        @submission_date AS job_date, -- // date
+        CURRENT_DATETIME() AS loaded_at, -- // datetime
 
     FROM
         `dev-fivetran.acoustic_sftp.raw_recipient_export`;
